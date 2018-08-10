0a9866f9e633114966c0c439391bfb6708494246
Alessandro Santamaria <a.reichgruber@gmail.com>
2018-08-10 17:04:04 +0200

    auth component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AuthComponent } from './auth/auth.component';
+import { ListErrorsComponent } from './shared/list-errors/list-errors.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    ListErrorsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+    MatInputModule,
+    MatAutocompleteModule,
+    MatDialogModule,
+    MatRippleModule,
+    MatProgressBarModule,
+    MatRadioModule
   ],
   providers: [],
   bootstrap: [AppComponent]
